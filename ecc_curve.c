590ac2364a871a9758059a9075f5151a5876ffcd
Raphael Barreto <rfsbarreto@gmail.com>
2014-02-07 12:20:44 -0300

    Add: Header file and Object file.

--- ecc_curve.c
+++ ecc_curve.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "ecc_curve.h"
 
-typedef struct ecc_point{
-	int x,y;
-} ecc_point;
+int init_curve(int a, int b,int prime, int order,int cofactor,ecc_point g){
+	a=a;
+	b=b;
+	prime=prime;
+	generator_point = g;
+	n= order;
+	h= cofactor;
+	if ( isPoint(g))
+		return 0;
+	else
+		return -1;
+}
 
-int a;
-ecc_point INFINITY_POINT;
-ecc_point* mult(ecc_point p, int value);
+
+int isPoint(ecc_point p){
+	return (p.y== sqrt(pow(p.x,3) + a*p.x + b));
+}
+
+
 int main(){}
 
 
@@ -17,7 +32,7 @@ ecc_point* sum(ecc_point p1,ecc_point p2){
 		result=mult(p1,2);
 	else
 		if( (p1.x==p2.x)&&(p2.y==-p1.y))
-			(*result)=INFINITY_POINT;
+			result=INFINITY_POINT;
 		else{
 			int s= (p1.y-p2.y)/(p1.x-p2.x);
 			(*result).x= s*s - p1.x - p2.x;
@@ -35,7 +50,7 @@ ecc_point* double_p(ecc_point p){
 		(*result).y=-p.y + s*(p.x-(*result).x);
 
 	}else
-		result=&INFINITY_POINT;
+		result=INFINITY_POINT;
 	return result;
 }
 
---
+++ ecc_curve.h
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+// Ponto de uma curva Elíptica
+typedef struct ecc_point{
+	int x,y;
+} ecc_point;
+
+
+//Parâmetros da curva ( variáveis globais
+
+//a,b são os coeficientes que definem a curva
+// prime é o número primo que define o tamanho(teto) do Grupo
+// n é a ordem da curva
+//h é o cofator  
+int a,b,prime,n,h;
+
+//Ponto gerador utilizado nos algoritmos do ECC
+ecc_point generator_point;
+
+// Definição como o ponto do Infinto sendo nulo
+ecc_point* INFINITY_POINT=NULL;
+
+//Função de inicialização dos Parâmetros da curva
+// Retorna 0 se foi concluída sem erros, -1 do contrário
+int init_curve(int a, int b, int prime, int order, int cofactor, ecc_point g);
+
+//Função de multiplicação de um ponto na curva por um escalar 
+//Retorna: Resultado da multiplicação, que também é um ponto da curva
+ecc_point* mult(ecc_point p, int value);
+
+
+//Função da soma de dois pontos da curva
+//Retorna: Resultado da soma, que também é um ponto da curva
+ecc_point* sum(ecc_point p1,ecc_point p2);
+
+//Função de verificação de Pontos.
+// retorna 1  se um dado ponto pertence à curva, 0 do contrário
+int isPoint(ecc_point p);
+
+//Função para dobrar(*2) um ponto da curva
+//Retorno: Dobro do ponto passado por parâmetro, que também é um ponto da curva
+ecc_point* double_p(ecc_point p);
